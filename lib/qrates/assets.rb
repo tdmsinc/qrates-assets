9894980fa7a32dcab2b0884c408ebcd35a978a18
Oleksii Vykhor <o.vykhor@ukr.net>
2020-05-28 12:20:46 +0300

    make assets to be working without rails

--- lib/qrates/assets.rb
+++ lib/qrates/assets.rb
@@ -2,7 +2,41 @@ require 'qrates/assets/version'
 
 module Qrates
   module Assets
-    class Engine < ::Rails::Engine
+    def self.gem_path
+      Pathname.new(File.dirname(__dir__))
+    end
+
+    def self.gem_spec
+      Gem::Specification::load(
+        gem_path.join('qrates-assets.gemspec').to_s
+      )
+    end
+
+    def self.load_paths
+      gem_path.join('../vendor/assets').each_child.to_a
+    end
+
+    def self.dependencies
+      []
+    end
+
+    if defined?(::Rails)
+      class Engine < ::Rails::Engine
+      end
     end
   end
 end
+
+class RailsAssets
+  @components ||= []
+
+  class << self
+    attr_accessor :components
+
+    def load_paths
+      components.flat_map(&:load_paths)
+    end
+  end
+end
+
+RailsAssets.components << Qrates::Assets
